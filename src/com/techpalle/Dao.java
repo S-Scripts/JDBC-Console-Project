0e5bfb7727c69a016c8dfba9e185704ca1954d89
S-Scripts <shreyahpadmanabha@gmail.com>
2024-10-29 20:04:46 +0530

    Dao.java

--- src/com/techpalle/Dao.java
+++ src/com/techpalle/Dao.java
@@ -17,17 +17,17 @@ public class Dao {
 	    try {
 	        Class.forName("com.mysql.cj.jdbc.Driver");
 	        
-	        c = DriverManager.getConnection("jdbc:mysql://localhost:3306/palle", "root", "root@123");
+	        c = DriverManager.getConnection("jdbc:mysql://localhost:3306/user_profiles", "root", "root@123");
 	        
 	        s = c.createStatement();
 	        
-	        // Check if the table already exists
-	        ResultSet rs = s.executeQuery("SHOW TABLES LIKE 'employee'");
+	        // To check if the table already exists
+	        ResultSet rs = s.executeQuery("SHOW TABLES LIKE 'employee_details'");
 	        if (rs.next()) {
-	            System.out.println("Note: The table 'employee' already exists in the database 'palle'.");
+	            System.out.println("Note: The table 'employee_details' already exists in the database 'user_profiles'. \n");
 	        } else {
-	            s.executeUpdate("CREATE TABLE employee (eid INT PRIMARY KEY, ename VARCHAR(40), esal INT)");
-	            System.out.println("Table 'employee' successfully created!");
+	            s.executeUpdate("CREATE TABLE employee_details (emp_id INT PRIMARY KEY, emp_fname VARCHAR(40), emp_lname VARCHAR(40), emp_phno VARCHAR(20), emp_city VARCHAR(20))");
+	            System.out.println("Table 'employee_details' successfully created! \n");
 	        }
 	    } 
 	    catch (ClassNotFoundException e) {
@@ -59,64 +59,59 @@ public class Dao {
 	}
 
 
-	public void createRow(int eid, String ename, int esal) {
+	public void createRow(int eid, String efname, String elname, String ephno, String ecity) {
+	  
+	    Connection c = null;
+	    PreparedStatement s = null;
 
-		
-		Connection c = null;
-		
-		PreparedStatement s = null;
-		
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			
-			c = DriverManager.getConnection("jdbc:mysql://localhost:3306/palle","root", "root@123");
-			
-			String query="insert into employee values(?,?,?)";
-			s = c.prepareStatement(query);
-			s.setInt(1, eid);
-			s.setString(2, ename);
-			s.setInt(3, esal);
-			
-			s.executeUpdate();
-			
-			System.out.println("Values added successfully! \n");
-		} 
-		catch (ClassNotFoundException e) {
-			System.out.println("Driver not loaded properly \n");
-			System.out.println(e.getCause());
-			System.out.println(e.getMessage());
-			e.printStackTrace();
-		} 
-		catch (SQLException e) {
-			System.out.println("Something went wrong with database \n");
-			e.printStackTrace();
-		}
-		catch (Exception e) {
-			System.out.println("Something unsually thing happened \n");
-			e.printStackTrace();
-		}
-		finally {
-			if(s!=null) {
-				try {
-					s.close();
-				} 
-				catch (SQLException e) {
-					e.printStackTrace();
-				}
-			}
-			if(c!=null) {
-				try {
-					c.close();
-				} 
-				catch (SQLException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-		
+	    try {
+	        Class.forName("com.mysql.cj.jdbc.Driver");
+	        c = DriverManager.getConnection("jdbc:mysql://localhost:3306/user_profiles", "root", "root@123");
+
+	        String query = "INSERT INTO employee_details VALUES (?, ?, ?, ?, ?)";
+	        s = c.prepareStatement(query);
+
+	        s.setInt(1, eid);
+	        s.setString(2, efname);
+	        s.setString(3, elname);
+	        s.setString(4, ephno);
+	        s.setString(5, ecity);
+
+	        s.executeUpdate();
+	        System.out.println("Values added successfully! \n");
+	    } 
+	    catch (ClassNotFoundException e) {
+	        System.out.println("Driver not loaded properly \n");
+	        e.printStackTrace();
+	    } 
+	    catch (SQLException e) {
+	        System.out.println("Something went wrong with database \n");
+	        e.printStackTrace();
+	    } 
+	    catch (Exception e) {
+	        System.out.println("Something unusual happened \n");
+	        e.printStackTrace();
+	    } 
+	    finally {
+	        if (s != null) {
+	            try {
+	                s.close();
+	            } catch (SQLException e) {
+	                e.printStackTrace();
+	            }
+	        }
+	        if (c != null) {
+	            try {
+	                c.close();
+	            } catch (SQLException e) {
+	                e.printStackTrace();
+	            }
+	        }
+	    }
 	}
 
-	public void updateRow(int eid, int newsal) {
+
+	public void updateRow(int eid, String newephno) {
 		
 		Connection c = null;
 		PreparedStatement s = null;
@@ -124,11 +119,11 @@ public class Dao {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			
-			c = DriverManager.getConnection("jdbc:mysql://localhost:3306/palle", "root", "root@123");
+			c = DriverManager.getConnection("jdbc:mysql://localhost:3306/user_profiles", "root", "root@123");
 			
-			String query = "update employee set esal = ? where eid = ?";
+			String query = "update employee_details set emp_phno = ? where emp_id = ?";
 			s = c.prepareStatement(query);
-			s.setInt(1, newsal);
+			s.setString(1, newephno);
 			s.setInt(2, eid);
 			
 			s.executeUpdate();
@@ -169,9 +164,9 @@ public class Dao {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			
-			c = DriverManager.getConnection("jdbc:mysql://localhost:3306/palle", "root", "root@123");
+			c = DriverManager.getConnection("jdbc:mysql://localhost:3306/user_profiles", "root", "root@123");
 			
-			String query = "delete from employee where eid = ?";
+			String query = "delete from employee_details where emp_id = ?";
 			s = c.prepareStatement(query);
 			s.setInt(1, eid);
 			
@@ -212,19 +207,20 @@ public class Dao {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			
-			c = DriverManager.getConnection("jdbc:mysql://localhost:3306/palle", "root", "root@123");
+			c = DriverManager.getConnection("jdbc:mysql://localhost:3306/user_profiles", "root", "root@123");
 			
 			s = c.createStatement();
 			
-			ResultSet res = s.executeQuery("select * from employee");
+			ResultSet res = s.executeQuery("select * from employee_details");
 			
 			System.out.println("Employee table data below \n");
-			System.out.println("eid  ename    esal \n");
+			System.out.println("Employee ID       First Name     Last Name      Phone Number      City \n");
+			System.out.println("---------------------------------------------------------------------- \n");
 			
 			while(res.next()) {
-				System.out.println(res.getInt(1)+ "  " + res.getString(2)+ "  " + res.getInt(3));
-			}
-			System.out.println("------------------------------------");
+				System.out.println(res.getInt(1)+ "               " + res.getString(2)+ "         " + res.getString(3)+ "            " + res.getString(4)+ "          " + res.getString(5));
+			} 
+			System.out.println("\n \n");
 		} 
 		catch (ClassNotFoundException e) {
 			e.printStackTrace();
@@ -259,9 +255,9 @@ public class Dao {
 	    
 	    try {
 	        Class.forName("com.mysql.cj.jdbc.Driver");
-	        c = DriverManager.getConnection("jdbc:mysql://localhost:3306/palle", "root", "root@123");
+	        c = DriverManager.getConnection("jdbc:mysql://localhost:3306/user_profiles", "root", "root@123");
 	        
-	        String query = "SELECT * FROM employee WHERE eid = ?";
+	        String query = "SELECT * FROM employee_details WHERE emp_id = ?";
 	        s = c.prepareStatement(query);
 	        s.setInt(1, eid);
 	        
@@ -269,7 +265,7 @@ public class Dao {
 	        
 	        if (res.next()) {
 	            System.out.println("Employee found:");
-	            System.out.println("eid: " + res.getInt(1) + ", ename: " + res.getString(2) + ", esal: " + res.getInt(3));
+	            System.out.println("employee_id: " + res.getInt(1) + ", first_name: " + res.getString(2) + ", last_name: " + res.getString(3) + ", phone_number: " + res.getString(4) + ", city: " + res.getString(5));
 	        } else {
 	            System.out.println("Employee with id " + eid + " not found.");
 	        }
@@ -300,4 +296,3 @@ public class Dao {
 	    }
 	}
 }
-
